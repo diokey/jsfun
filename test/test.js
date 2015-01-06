6c1395de7d95bb2d49cef5e024913a71d6032e6e
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-01-06 00:53:24 -0500

    Implemented and tested unary function :)

--- test/test.js
+++ test/test.js
@@ -95,3 +95,44 @@ describe('Curly function', function() {
     }); 
 });
 
+describe('Unary operator', function() {
+
+    function unary(func) {
+        //check if the argument is a function
+        if(typeof func !== 'function') {
+            throw new Error('Function please');
+        }
+        return function (arg) {
+            return func.apply(null,[arg]); 
+        };
+    }
+
+    it('should return a function',function(){
+        //check if it returns a function
+        assert.equal(typeof unary(parseInt),'function'); 
+    });
+
+    it('Should throw an exception when not passed a function as argument',function(){
+        
+        //check if it handles case when argument is not a function
+        assert.throws(function() { 
+            var test = 'some random text';
+            unary(test)(7);
+        }, Error );
+    });
+
+    it('Unary function should accept only one argument', function() {
+      
+
+        //check if it only takes the firs argument
+        var fun = unary(parseInt);
+        assert.equal(fun('7'),7);
+        assert.equal(fun('5',2),5);
+        assert.equal(parseInt('A',16),10);
+    });
+
+    it('should implement unary operator passing "parseInt" only one argument', function() {
+        assert.deepEqual(['1','2','3'].map(unary(parseInt)),[1,2,3]);  
+    });
+});
+
